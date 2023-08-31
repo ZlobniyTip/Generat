c7146b42c36d7e332ad3c8f5168a6c2181852a8d
ZlobniyTip <4opick_97@mail.ru>
2023-08-31 18:49:14 +0300

    Corrected

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -14,13 +14,37 @@ public class Spawn : MonoBehaviour
         _createEnemyJob = StartCoroutine(CreateEnemy());
     }
 
+    private void RestartCoroutine()
+    {
+        StopCoroutine(CreateEnemy());
+        _createEnemyJob = StartCoroutine(CreateEnemy());
+    }
+
     private IEnumerator CreateEnemy()
     {
+        var waitForTwoSeconds = new WaitForSeconds(2);
+
         for (int i = 0; i < _transform.childCount; i++)
         {
             _points[i] = _transform.GetChild(i);
             Instantiate(_enemy, _points[i].transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(2);
+            yield return waitForTwoSeconds;
         }
+
+        RestartCoroutine();
+    }
+
+    public Vector3 SetDirection()
+    {
+        int random = Random.Range(0, 2);
+
+        if (random == 1)
+        {
+            Debug.Log("Left");
+            return Vector3.left;
+        }
+
+        Debug.Log("Rigth");
+        return Vector3.right;
     }
 }
